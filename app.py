9d3ca56560b466fb912454807445f210f0a97a16
ekuin0x <salhimouiz1@gmaiL.com>
2023-12-20 17:40:08 +0100

    wwhhhw

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from deepcorrect import DeepCorrect
 
 app = Flask(__name__)
 @app.route("/")
 def index() :
-    return "HELLLO"
+    with open("text.txt", "r") as f :
+        txt = f.read()
+        corrector = DeepCorrect('params_path', 'checkpoint_path')
+        return corrector.correct(txt)
 
 if __name__ == "__main__" :
     app.run(port=5000, debug=False)
